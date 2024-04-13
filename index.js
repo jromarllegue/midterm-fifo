ae331135257fb57147db9de65a7ca0a1c84a6f6b
jromarllegue <lleguejohnromar02@gmail.com>
2024-04-13 11:57:47 +0800

    new

--- index.js
+++ index.js
@@ -1,51 +1,64 @@
-let text = "";
+function runFIFO() {
+    let pageList = $("#pageList").val().split(" ");
+    let pageSequence = $("pageSequence").val().split(", ");
+    let frames = new Array(frameCount).fill("");
+  
+    let output = "<h3>Output:</h3><table>";
+    output += "<tr><th>Sequence</th>";
+  
+    for (let i = 0; i < numFrames; i++) {
+      output += "<th>F" + (i + 1) + "</th>";
+    }
+    output += "</tr>";
 
-text = "How many page? "
-let page = Number(prompt(text));
-body.innerHTML += text + page + "<br>";
-console.log(page);
-
-let pagearray = [];
-for (let i = 1; i <= page; i++) {
-    text = "Page " + i + " name: ";
-    pagearray[i-1] = prompt(text);
-    body.innerHTML += text + pagearray[i-1] + "<br>";
-}
-body.innerHTML += "<br>";
-console.log(pagearray);
-
-text = "How many sequence? "
-let sequence = Number(prompt(text));
-body.innerHTML += text + sequence + "<br>";
-console.log(sequence);
-
-let sequencearray = [];
-for (let i = 1; i <= sequence; i++) {
-    text = "Sequence " + i + " name: ";
-    sequencearray[i-1] = prompt(text);
-    body.innerHTML += text + sequencearray[i-1] + "<br>";
-}
-body.innerHTML += "<br>";
-console.log(sequencearray);
-
-text = "How many frames? "
-let frame = Number(prompt(text));
-body.innerHTML += text + frame + "<br><br>";
-console.log(frame);
-
-body.innerHTML += "<table id='table'></table>";
-let table = document.getElementById("table");
-
-table.innerHTML +=  "<tr id='rowHead'></tr>";
-let rowHead = document.getElementById("rowHead");
-
-rowHead.innerHTML += "<th> </th>";
-for (let i = 0; i < sequencearray.length; i++) {
-    rowHead.innerHTML += "<th>" + sequencearray[i] + "</th>";
-}
-
-for (let i = 1; i <= frame; i++) {
-    table.innerHTML +=  "<tr id='f" + i +"'>" + 
-                            "<th> F"+ i +"</th>" + 
-                        "</tr>";
-}
+    let pageFaults = 0;
+    let pageSuccess = 0;
+    for (let j = 0; j < pageSequence.length; j++) {
+      let page = pageSequence[j];
+      let pageFound = false;
+      let pageFault = false;
+  
+      if (!pageList.includes(page)) {
+        alert("Error: '" + page + "'");
+        return;
+      }
+  
+      for (let k = 0; k < numFrames; k++) {
+        if (frames[k] === page) {
+          pageFound = true;
+          pageSuccess++;
+          break;
+        }
+      }
+  
+      if (!pageFound) {
+        pageFaults++;
+        pageFault = true;
+  
+        for (let l = 0; l < numFrames - 1; l++) {
+          frames[l] = frames[l + 1];
+        }
+        frames[numFrames - 1] = page;
+      }
+  
+      output += "<tr><td>" + page + "</td>";
+      for (let m = 0; m < numFrames; m++) {
+        output += "<td>" + (pageFault && frames[m] === "" ? "-" : frames[m]) + "</td>";
+      }
+      output += "</tr>";
+    }
+  
+    output += "</table>";
+  
+    let pageFaultPercentage = (pageFaults / pageSequence.length * 100).toFixed(2);
+    let pageSuccessPercentage = ((pageSequence.length - pageFaults) / pageSequence.length * 100).toFixed(2);
+  
+    output += "<p>Success Rate = " + (pageSequence.length - pageFaults) + "/" + pageSequence.length + "<br>";
+    output += "= " + pageSuccessPercentage + "%<br><br>";
+  
+    output += "Fault Rate = " + pageFaults + "/" + pageSequence.length + "<br>";
+    output += "= " + pageFaultPercentage + "%</p>";
+  
+    document.getElementById("output").innerHTML = output;
+  }
+  
